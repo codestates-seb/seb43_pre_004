421843ce0310d93013ad187118c2dfcd7b07033b
King0wl <70518135+yhj0076@users.noreply.github.com>
2023-04-15 16:12:55 +0900

    feat: add ExceptionCode.java
    위 코드는 에러코드를 자세히 정의하기 위한 코드입니다. 이 부분은 백엔드와 공유가 가능할 듯 싶습니다.

---
+++ backend/src/main/java/com/SOF/backend/exception/ExceptionCode.java
@@ -0,0 +1,27 @@
+/*
+ * 파일명 : ExceptionCode.enum
+ * 목적 : 에러코드를 더 자세히 알리기 위해서 설정한 enum.
+ * 파일 생성일 : 2023.04.15
+ * 마지막 편집일 : 2023.04.15
+ * 마지막 편집자 : 이호준
+ *
+ * 해당 코드는 codestates의 SEB BE과정 Section 3의 예외 처리 코드에서 가져왔음을 알립니다.
+ * */
+package com.SOF.backend.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    MEMBER_NOT_FOUND(404, "Member Not Found");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
