ef7175f3178843a992e81bcef29102c0411fa120
King0wl <70518135+yhj0076@users.noreply.github.com>
2023-04-15 16:13:27 +0900

    feat: add MemberController.java
    이 커밋은 멤베 컨트롤러를 추가한 것입니다.

---
+++ backend/src/main/java/com/SOF/backend/member/controller/MemberController.java
@@ -0,0 +1,12 @@
+/*
+ * 파일명 : MemberController.class
+ * 목적 : URI를 연결해주는 컨트롤러
+ * 파일 생성일 : 2023.04.15
+ * 마지막 편집일 : 2023.04.15
+ * 마지막 편집자 : 이호준
+ *
+ * */
+package com.SOF.backend.member.controller;
+
+public class MemberController {
+}
