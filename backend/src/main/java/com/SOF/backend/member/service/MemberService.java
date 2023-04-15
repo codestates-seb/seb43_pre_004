fcbc9eee20ed3f0f66c64656e6a57b36de3fd444
King0wl <70518135+yhj0076@users.noreply.github.com>
2023-04-15 23:04:20 +0900

    feat: 추가 findmembers, 추가 deleteMembers
    CRUD 모든 걸 끝냈습니다.

--- backend/src/main/java/com/SOF/backend/member/service/MemberService.java
+++ backend/src/main/java/com/SOF/backend/member/service/MemberService.java
@@ -11,6 +11,9 @@ import com.SOF.backend.exception.BusinessLogicException;
 import com.SOF.backend.exception.ExceptionCode;
 import com.SOF.backend.member.Entity.Member;
 import com.SOF.backend.member.repository.MemberRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -45,7 +48,16 @@ public class MemberService {
         return findVerifiedMember(memberId);
     }
 
+    public Page<Member> findMembers(int page, int size){
+        return memberRepository.findAll(PageRequest.of(page,size,
+                Sort.by("memberId").descending()));
+    }
 
+    public void deleteMember(long memberId){
+        Member findMember = findVerifiedMember(memberId);
+
+        memberRepository.delete(findMember);
+    }
 
     private Member findVerifiedMember(Long memberId) {
         Optional<Member> optionalMember =
