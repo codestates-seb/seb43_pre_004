42b60a53d94bdc8038fbecdc4f97754998ff5c1d
King0wl <70518135+yhj0076@users.noreply.github.com>
2023-04-15 16:15:48 +0900

    feat: add MemberService.java
    서비스 계층 구현입니다. 아직 CRUD중 CU만 구현되었고 R은 구현중입니다. 멤버 페이지네이션부터 처리하면 됩니다.

---
+++ backend/src/main/java/com/SOF/backend/member/service/MemberService.java
@@ -0,0 +1,58 @@
+/*
+ * 파일명 : MemberService.class
+ * 목적 : 회원의 CRUD 관리하기 위한 클래스
+ * 파일 생성일 : 2023.04.15
+ * 마지막 편집일 : 2023.04.15
+ * 마지막 편집자 : 이호준
+ * */
+package com.SOF.backend.member.service;
+
+import com.SOF.backend.exception.BusinessLogicException;
+import com.SOF.backend.exception.ExceptionCode;
+import com.SOF.backend.member.Entity.Member;
+import com.SOF.backend.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Member createMember(Member member){
+        return memberRepository.save(member);
+    }
+
+    public Member updateMember(Member member){
+        Member findMember = findVerifiedMember(member.getMemberId());
+
+        Optional.ofNullable(member.getNickname())
+                .ifPresent(nickname -> findMember.setNickname(nickname));
+        Optional.ofNullable(member.getImage())  // image를 byte 배열로 받아야한다.
+                .ifPresent(image -> findMember.setImage(image));
+
+        findMember.setLatestLog(LocalDateTime.now());
+
+        return memberRepository.save(findMember);
+    }
+
+    public Member findMember(long memberId){
+        return findVerifiedMember(memberId);
+    }
+
+
+
+    private Member findVerifiedMember(Long memberId) {
+        Optional<Member> optionalMember =
+                memberRepository.findById(memberId);
+        Member findMember =
+                optionalMember.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        return findMember;
+    }
+}
