c2692c05627653aefb5591bd821b8fadd3c9b7d7
Gomuang <97213554+NewfileDOTpy@users.noreply.github.com>
2023-04-17 11:41:00 +0900

    feat : 추가 Question Entity
    Question Entity V1 완료

--- backend/src/main/java/com/SOF/backend/question/Question.java
+++ backend/src/main/java/com/SOF/backend/question/Question.java
@@ -1,4 +1,39 @@
 package com.SOF.backend.question;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Blob;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Question {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+
+    private String title;
+
+    private String content;
+
+    private Blob ContentImg;
+
+    private int viewed;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime modifiedAt = LocalDateTime.now();
 }
