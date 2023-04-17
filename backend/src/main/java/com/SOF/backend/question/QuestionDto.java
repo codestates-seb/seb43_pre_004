aba3b72056ca9eda14f1fde2b689273552786f89
Gomuang <97213554+NewfileDOTpy@users.noreply.github.com>
2023-04-17 13:09:38 +0900

    feat : 추가 Question 통합 Dto V1
    Question 통합 Dto V1 완료

--- backend/src/main/java/com/SOF/backend/question/QuestionDto.java
+++ backend/src/main/java/com/SOF/backend/question/QuestionDto.java
@@ -1,4 +1,64 @@
 package com.SOF.backend.question;
 
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NonNull;
+import org.springframework.lang.Nullable;
+
+import java.sql.Blob;
+import java.time.LocalDateTime;
+
+
 public class QuestionDto {
+
+
+    @Getter
+    public static class Create {
+
+
+        @NotNull
+        private String title;
+
+        @NotNull
+        private String content;
+
+        @Nullable
+        private Blob contentImg;
+
+    }
+
+
+
+    @Getter
+    public static class Update {
+
+        private String title;
+
+        private String content;
+
+        private Blob contentImg;
+    }
+
+
+    @Getter
+    public static class response{
+
+        private Long questionId;
+
+        private String title;
+
+        private String content;
+
+        private Blob contentImg;
+
+        private LocalDateTime createdAt;
+
+        private LocalDateTime modifiedAt;
+
+
+    }
+
+
+
+
 }
