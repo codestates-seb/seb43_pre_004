5485cf34824b4f720cf41b7633e4929cefb39f03
UHA <110329914+yuhallo@users.noreply.github.com>
2023-04-17 14:58:29 +0900

    Feat(header) : 추가 header component

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,14 @@
+import GlobalStyles from './styles/GlobalStyles';
+import Search from './components/Search';
+import Header from './components/Header';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyles />
+      <Header />
+      <Login />
     </div>
   );
 }
