fc32b7126bd1e12043edf88f87aed3cbf5279297
Dongholy <dongholy@naver.com>
2023-04-18 10:15:49 +0900

    feat(AskQuestion):추가 pages(AskQuestion) + components(TextEditor)

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AskQuestion from './pages/AskQuestion';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/askquestion" element={<AskQuestion />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
