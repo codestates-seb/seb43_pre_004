fab8fd9cbf7515e4c9466c2db24a71702b7b32a1
UHA <110329914+yuhallo@users.noreply.github.com>
2023-04-18 14:00:53 +0900

    style(package) : 추가 npm-package

--- front/src/App.js
+++ front/src/App.js
@@ -3,12 +3,14 @@ import Search from './components/Search';
 import Header from './components/Header';
 import Login from './pages/Login';
 import Footer from './components/Footer';
+import AskQuestion from './pages/AskQuestion';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
       <Header />
+      <AskQuestion />
       <Footer />
     </div>
   );
