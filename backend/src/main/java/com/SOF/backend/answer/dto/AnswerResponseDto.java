a428e2794183ea11c2cdb14fff0ff364079f5ea8
min4415 <csm4415@gmail.com>
2023-04-19 18:30:07 +0900

    연관관계 매핑 오류 수정

--- backend/src/main/java/com/SOF/backend/answer/dto/AnswerResponseDto.java
+++ backend/src/main/java/com/SOF/backend/answer/dto/AnswerResponseDto.java
@@ -1,5 +1,8 @@
 package com.SOF.backend.answer.dto;
 
+import com.SOF.backend.answer.entity.Answer;
+import com.SOF.backend.member.Entity.Member;
+import com.SOF.backend.question.Question;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +21,7 @@ public class AnswerResponseDto {
     private String content;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
-    private Long questionId;
-    private Long memberId;
+    private Question questionId;
+    private Member memberId;
 }
 
