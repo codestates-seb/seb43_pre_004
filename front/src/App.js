2a090c187c67c05e65c472db76e5b10bb9d164cf
UHA <110329914+yuhallo@users.noreply.github.com>
2023-04-19 10:22:26 +0900

    Remame(header): 수정 폴더구조

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import GlobalStyles from './styles/GlobalStyles';
-import Search from './components/Search';
-import Header from './components/Header';
+import Search from './components/header/Search';
+import Header from './components/header/Header';
 import Login from './pages/Login';
 import Footer from './components/Footer';
 import AskQuestion from './pages/AskQuestion';
@@ -13,7 +13,6 @@ function App() {
     <div className="App">
       <GlobalStyles />
       <Header />
-
       <Footer />
     </div>
   );
