8ad15f15d0dae0678435569111503c30810e4537
King0wl <70518135+yhj0076@users.noreply.github.com>
2023-04-20 11:46:02 +0900

    feat: 패스워드 암호화 전 JWT 작업

--- backend/src/main/java/com/SOF/backend/member/Entity/Member.java
+++ backend/src/main/java/com/SOF/backend/member/Entity/Member.java
@@ -36,7 +36,7 @@ public class Member {
     @Column(nullable = false, unique = true)
     private String nickname;
 
-    @Column(nullable = false)
+    @Column(length = 100, nullable = false)
     private String password;
 
     @Column
