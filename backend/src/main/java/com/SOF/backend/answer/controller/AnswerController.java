29d1ea07c34f18f48363d810091c301a9cd4b485
min4415 <csm4415@gmail.com>
2023-04-25 10:36:51 +0900

    answer 연관관계 매핑 완료

--- backend/src/main/java/com/SOF/backend/answer/controller/AnswerController.java
+++ backend/src/main/java/com/SOF/backend/answer/controller/AnswerController.java
@@ -1,11 +1,13 @@
 package com.SOF.backend.answer.controller;
 
+import com.SOF.backend.Utils.ContextHolederUtils;
 import com.SOF.backend.answer.dto.AnswerPatchDto;
 import com.SOF.backend.answer.dto.AnswerPostDto;
-import com.SOF.backend.answer.dto.AnswerResponseDto;
 import com.SOF.backend.answer.entity.Answer;
 import com.SOF.backend.answer.mapper.AnswerMapper;
 import com.SOF.backend.answer.service.AnswerService;
+import com.SOF.backend.question.QuestionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,24 +17,30 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/answer")
+@Slf4j
 @Validated
 public class AnswerController {
     private final AnswerService answerService;
     private final AnswerMapper answerMapper;
+    private final ContextHolederUtils contextHolederUtils;
+    private final QuestionService questionService;
 
 
-    public AnswerController(AnswerService answerService, AnswerMapper answerMapper) {
+    public AnswerController(AnswerService answerService, AnswerMapper answerMapper, ContextHolederUtils contextHolederUtils, QuestionService questionService) {
         this.answerService = answerService;
         this.answerMapper = answerMapper;
+        this.contextHolederUtils = contextHolederUtils;
+        this.questionService = questionService;
     }
 
     //답변 추가하기
     @PostMapping("/{post-id}")
     public ResponseEntity postAnswer(@PathVariable("post-id") Long postId,
                                      @Valid @RequestBody AnswerPostDto answerPostDto){
+        Long memberId = contextHolederUtils.getAuthMemberId();
         Answer answer = answerMapper.answerPostDtoToAnswer(answerPostDto);
 
-        Answer response = answerService.createAnswer(answer);
+        Answer response = answerService.createAnswer(postId,memberId,answer);
 
         return new ResponseEntity<>(answerMapper.answerToAnswerResponseDto(response), HttpStatus.CREATED);
     }
@@ -43,10 +51,11 @@ public class AnswerController {
                                       @PathVariable("answer-id") Long answerId,
                                       @Valid @RequestBody AnswerPatchDto answerPatchDto){
         answerPatchDto.setAnswerId(answerId);
+        Long memberId = contextHolederUtils.getAuthMemberId();
 
         Answer answer = answerMapper.answerPatchDtoToAnswer(answerPatchDto);
 
-        Answer response = answerService.updateAnswer(answer);
+        Answer response = answerService.updateAnswer(memberId,postId,answerId,answer);
 
         return new ResponseEntity(answerMapper.answerToAnswerResponseDto(response),HttpStatus.OK);
     }
@@ -63,7 +72,10 @@ public class AnswerController {
     @DeleteMapping("/{post-id}/{answer-id}")
     public ResponseEntity deleteAnswer(@PathVariable("post-id") Long postId,
                                        @PathVariable("answer-id") Long answerId){
-        answerService.deleteAnswer(answerId);
+
+        Long memberId = contextHolederUtils.getAuthMemberId();
+
+        answerService.deleteAnswer(memberId,postId,answerId);
 
         return ResponseEntity.noContent().build();
     }
