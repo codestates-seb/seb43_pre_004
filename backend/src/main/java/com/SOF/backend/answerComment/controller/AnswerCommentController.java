74ee263765c2b80feb44741e2b93efc74fd40ff7
min4415 <csm4415@gmail.com>
2023-04-25 10:35:29 +0900

    answerComment 연관관계 매핑 완료

--- backend/src/main/java/com/SOF/backend/answerComment/controller/AnswerCommentController.java
+++ backend/src/main/java/com/SOF/backend/answerComment/controller/AnswerCommentController.java
@@ -1,8 +1,8 @@
 package com.SOF.backend.answerComment.controller;
 
+import com.SOF.backend.Utils.ContextHolederUtils;
 import com.SOF.backend.answerComment.dto.AnswerCommentPatchDto;
 import com.SOF.backend.answerComment.dto.AnswerCommentPostDto;
-import com.SOF.backend.answerComment.dto.AnswerCommentResponseDto;
 import com.SOF.backend.answerComment.entity.AnswerComment;
 import com.SOF.backend.answerComment.mapper.AnswerCommentMapper;
 import com.SOF.backend.answerComment.service.AnswerCommentService;
@@ -19,19 +19,22 @@ import javax.validation.Valid;
 public class AnswerCommentController {
     private final AnswerCommentMapper answerCommentMapper;
     private final AnswerCommentService answerCommentService;
+    private final ContextHolederUtils contextHolederUtils;
 
-    public AnswerCommentController(AnswerCommentMapper answerCommentMapper, AnswerCommentService answerCommentService) {
+    public AnswerCommentController(AnswerCommentMapper answerCommentMapper, AnswerCommentService answerCommentService, ContextHolederUtils contextHolederUtils) {
         this.answerCommentMapper = answerCommentMapper;
         this.answerCommentService = answerCommentService;
+        this.contextHolederUtils = contextHolederUtils;
     }
 
     //답변에 댓글 추가하기
     @PostMapping("/{answer-id}")
     public ResponseEntity postAnswerComment(@PathVariable("answer-id") Long answerId,
                                             @Valid @RequestBody AnswerCommentPostDto answerCommentPostDto){
+        Long memberId = contextHolederUtils.getAuthMemberId();
         AnswerComment answerComment = answerCommentMapper.answerCommentPostDtoToAnswerComment(answerCommentPostDto);
 
-        AnswerComment response = answerCommentService.createAnswerComment(answerComment);
+        AnswerComment response = answerCommentService.createAnswerComment(memberId,answerId,answerComment);
 
         return new ResponseEntity<>(answerCommentMapper.answerCommentToAnswerCommentResponseDto(response), HttpStatus.CREATED);
     }
@@ -43,9 +46,11 @@ public class AnswerCommentController {
                                              @Valid @RequestBody AnswerCommentPatchDto answerCommentPatchDto){
         answerCommentPatchDto.setAnswerCommentId(answerCommentId);
 
+        Long memberId = contextHolederUtils.getAuthMemberId();
+
         AnswerComment answerComment = answerCommentMapper.answerCommentPatchDtoToAnswerComment(answerCommentPatchDto);
 
-        AnswerComment response = answerCommentService.updateAnswerComment(answerComment);
+        AnswerComment response = answerCommentService.updateAnswerComment(memberId,answerId,answerComment);
 
         return new ResponseEntity<>(answerCommentMapper.answerCommentToAnswerCommentResponseDto(response),HttpStatus.OK);
     }
@@ -54,7 +59,10 @@ public class AnswerCommentController {
     @DeleteMapping("/{answer-id}/{answerComment-id}")
     public ResponseEntity deleteAnswerComment(@PathVariable("answer-id") Long answerId,
                                               @PathVariable("answerComment-id") Long answerCommentId){
-        answerCommentService.deleteAnswerComment(answerCommentId);
+
+        Long memberId = contextHolederUtils.getAuthMemberId();
+
+        answerCommentService.deleteAnswerComment(memberId,answerId,answerCommentId);
 
         return ResponseEntity.noContent().build();
     }
