6b25c99ea246ab9a87a1af3e17705525d011d88f
min4415 <csm4415@gmail.com>
2023-04-25 10:37:52 +0900

    answer 최종 수정

--- backend/src/main/java/com/SOF/backend/answer/repository/AnswerRepository.java
+++ backend/src/main/java/com/SOF/backend/answer/repository/AnswerRepository.java
@@ -3,5 +3,6 @@ package com.SOF.backend.answer.repository;
 import com.SOF.backend.answer.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface AnswerRepository extends JpaRepository<Answer,Long> {
 }
